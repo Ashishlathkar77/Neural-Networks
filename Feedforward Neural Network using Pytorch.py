2652ccf70137dd51cf58e7a65bda58b1a2920cf3
Ashish Lathkar <63287421+Ashishlathkar77@users.noreply.github.com>
2024-01-29 05:52:31 +0530

    Feedforward Neural Network using Pytorch.py

--- Feedforward Neural Network using Pytorch.py
+++ Feedforward Neural Network using Pytorch.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
